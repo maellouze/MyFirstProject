c8216cd989c0e1a77129f5e78d8f20f98917e904
Mohamed Amine ELLOUZE <maellouze@vermeg.com>
2021-06-03 13:23:14 +0200

    commit Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld 
+{ 
+// Votre programme commence par main(). 
+// Écrire "Hello, World" dans le terminal de window. 
+public static void main(String args[]) 
+{ 
+System.out.println("Hello, World"); 
+} 
+}
